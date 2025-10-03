e3e190afb5a1e80cf1294d398120a1d5319876ff
Danny Haro <haro82507@gmail.com>
2025-10-02 23:01:13 -0500

    actualizando credenciales de BD

--- src/db.js
+++ src/db.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql2');
 
 // Configuración de la conexión a MySQL
-const connection = mysql.createConnection({
+/*const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '123',
   database: 'cloud_matriculadb'
+});*/
+
+// Configuración de la conexión al servidor Mariadb
+const connection = mysql.createConnection({
+  host: '192.168.99.10',
+  user: 'admin',
+  password: '123',
+  database: 'cloudmatriculadb'
 });
 
 // Conexión
